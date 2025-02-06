5f165805a4e73c6d18c48133793c5484dee03115
Syava <syavaopryshko@gmail.com>
2025-02-06 16:03:25 +0200

    SearchBar2

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,50 +1,38 @@
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import css from "./SearchBar.module.css";
-import { FaMagnifyingGlass } from "react-icons/fa6";
-import toast, { Toaster } from "react-hot-toast";
-import { useState } from "react";
 
-export default function SearchBar({ onSubmit }) {
-  const [searchQuery, setSearchQuery] = useState("");
+function SearchBar({ onSearch }) {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const form = evt.target;
+    const topic = form.elements.topic.value.trim();
 
-  const handleChange = (e) => {
-    setSearchQuery(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!searchQuery.trim()) {
-      return toast.error("Cannot be empty");
+    if (topic === "") {
+      toast.error("Please enter a search term!", {
+        position: "top-right",
+        autoClose: 3000,
+      });
+      return;
     }
-    onSubmit(searchQuery);
-    setSearchQuery("");
+
+    onSearch(topic);
+    form.reset();
   };
 
   return (
-    <header className={css.searchBarContainer}>
-      <Toaster
-        position="top-right"
-        reverseOrder={false}
-        toastOptions={{
-          duration: 5000,
-          style: {
-            fontFamily: "sans-serif",
-          },
-        }}
-      />
-      <form className={css.searchForm} onSubmit={handleSubmit}>
-        <button type="submit" className={css.searchBtn}>
-          <FaMagnifyingGlass />
-        </button>
+    <>
+      <form onSubmit={handleSubmit} className={css.searchForm}>
         <input
           type="text"
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-          value={searchQuery}
-          onChange={handleChange}
-          className={css.searchInput}
+          name="topic"
+          placeholder="Search photos..."
+          className={css.inputSearch}
         />
+        <button type="submit">Search</button>
       </form>
-    </header>
+    </>
   );
 }
+
+export default SearchBar;
